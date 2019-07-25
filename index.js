1eadc45364c615cba11cf81fcea00345d5dc0146
Katrina R <katrina.reinsdorff@gmail.com>
2019-07-25 18:57:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// write your code below!
+// // write your code below!
 
-var name = "Katrina"
-var height = 74
+// var name = "Katrina"
+// var height = 74
 
-console.log("Name:")
-console.log(name)
+// console.log("Name:")
+// console.log(name)
 
-console.log("Height:")
-console.log(height-9)
+// console.log("Height:")
+// console.log(height-9)
